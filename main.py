8aa10b2067dac837f41dc1fac5d053bf21988fbf
Wills_Theodore <willt502@hsd.k12.or.us>
2022-04-28 11:22:14 -0700

    <reworked all parts and added player stats>

--- main.py
+++ main.py
@@ -1,22 +1,60 @@
 import sys
-import random
 # def main(args: list[str]) -> int:
 #     # print(args)
-def enemylvlone
-    slime = 1
-    skelton = 2
-    goblin = 3
+enemies = {
+    1 : "slime",
+    2 : "skelton",
+    3 : "goblin",
+}
 
-def randomnum():
-    picknum = random.randint(1,3)
-    print(picknum)
-    if picknum == 1:
-        print("slime")
-    elif picknum == 2:
-        print("skelton")
-    elif picknum == 3:
-        print("goblin")
+level = 1
+import random 
+enemy =  random.randint(1,3)
+picknum = enemy
+print(picknum)
+enemy = enemies[enemy]
+print("Enemy type:" , enemy)
 
-randomnum()
+def player_stats():
+    print("Player Stats: ")
+    player_health()
+    player_armour()
+    player_ap()
+
+def player_health():
+    max_health = 50
+    print("Your health:" , max_health)
+
+def player_armour():
+    current_armour = 10
+    print("Your armour strentgh:" , current_armour)
+
+def player_ap():
+    attack_points = 10
+    print("Your damage:" , attack_points)
+
+
+def enemydisplay():
+    enemyhealth()
+    enemyarmour() 
+    enemyap() 
+    
+def enemyhealth():
+    print("Enemys health:" , level * 10 * picknum)
+
+def enemyarmour():
+    print("Enemys armour:" , level * 5 * picknum)
+    
+def enemyap():
+    print("Enemys attack points:" , level * 3 * picknum)
+
+def playerattack():
+    enemydisplay()
+
+def tempname():  
+    playerattack()
+
+tempname()
+player_stats()
 #   if __name__ == "__main__":
 #     error: int = main(sys.argv)
